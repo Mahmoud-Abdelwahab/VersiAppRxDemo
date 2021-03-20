469f07d504aa01f3cc6959cc78839e12d11ea75a
Mahmoud-Abdelwahab <mahmoudabdelwahab199390@gmail.com>
2021-03-20 19:21:46 +0200

    Initial Commit

---
+++ VersiAppRxDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VersiAppRxDemo
+//
+//  Created by Mahmoud Abdul-Wahab on 20/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
